c2f8b8c76138700d34c4686db60817369e56e4e1
aaronberkhoff <aaronberkhoff@outlook.com>
2025-06-02 13:26:20 -0600

    Added state object and unit test

---
+++ src/monolith/states/__init__.py
@@ -0,0 +1,63 @@
+"""
+State object definition and useful functions
+
+"""
+
+from collections.abc import Iterable
+
+import torch
+
+ECI = "J2000"
+ECEF = "ITRF93"
+
+class State:
+    """
+    Object that stores state information:
+
+    Attributes:
+    ----------
+        position: torch tensor (3,1), optional
+        velocity: torch tensor (3,1), optional
+        acceleration: torch tensor (3,1), optional
+        attitude: torch tensor (4,1) or (3,1), optional
+        angular_velocity: torch tensor (3,1), optional
+        angular_acceleration: torch tensor (3,1), optional
+        latitude: torch tensor (1,1)
+        longitude: torch tensor (1,1)
+        altitude: torch tensor
+        time: datetime, optional
+        frame: str, optional:
+            The reference frame of the state ('inertial' or 'ECEF', default is 'inertial').
+        orbital_elements: monolith.orbital_elements, optional
+
+    """
+
+    _supported_attributes = ['position', 'velocity', 'acceleration', 'time','lat']
+
+    def __init__(self, frame = "inertial",metadata = {},**kwargs):
+
+        """
+        Constructs all the necessary attributes for the State object.
+        """
+        for key, value in kwargs.items():
+
+            if key in State._supported_attributes:
+
+                if isinstance(value,(Iterable,float,int)):
+                    value =  torch.tensor(value).view(3,1)
+
+                setattr(self, key, value)
+            else:
+                raise ValueError(f"kwarg <{key}> is not supported")
+
+            
+    
+
+
+
+
+
+
+
+
+        
---
+++ tests/states/test_unit.py
@@ -0,0 +1,46 @@
+"""
+Unit test for the states folder
+
+"""
+import numpy as np
+from datetime import datetime
+import torch
+
+from monolith.states import State
+
+
+def test_state_creation():
+
+    """
+    Test the creation of the State object
+    """
+
+    position = (1,1,1)
+    velocity = (1,1,1)
+    date_str = "2025-06-02"
+    time = datetime.strptime(date_str, "%Y-%m-%d")
+
+    state = State(position = position, velocity = velocity, time = time)
+
+    #check the right dimensions
+    assert state.position.shape == (len(position),1)
+    assert state.velocity.shape == (len(velocity),1)
+
+    #check if they are the right type and data is the same
+    assert state.time == time
+    assert torch.equal(state.position, torch.tensor(position).view(3,1))
+    assert torch.equal(state.velocity, torch.tensor(velocity).view(3,1))
+
+
+
+if __name__ == "__main__":
+
+    test_state_creation()
+
+    
+
+
+
+
+    
+
